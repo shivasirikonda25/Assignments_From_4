350578070e3fd807be1a849ae665f4e9181453c6
Sirikonda Shiva <Sirikonda.Shiva@godigit.com>
2024-08-01 12:32:34 +0530

    [SHIVA] 1st project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        SetBehaviour sb=new SetBehaviour();
-        sb.setbeh();
+        System.out.println("Hello world!");
     }
 }
---
+++ src/Question1.java
@@ -0,0 +1,4 @@
+public interface Question1
+{
+    public int sum(int a,int b);
+}
