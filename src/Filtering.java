c75998aef805dc28aeb273c10ab9194b4f13b047
Sirikonda Shiva <Sirikonda.Shiva@godigit.com>
2024-08-01 14:38:29 +0530

    Problem2

--- src/Filtering.java
+++ src/Filtering.java
@@ -1,4 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Filtering
 {
+   public static void main(String[] args)
+   {
+       List<String> l=new ArrayList<>();
+       Scanner sc=new Scanner(System.in);
+       while(true)
+       {
+           System.out.println("enter a String in the list : enter -1 to exit" );
+           String c1=sc.next();
+           if(c1.equals("-1")) break;
+           l.add(c1);
+       }
+       List<String> l2=l.stream().filter(c->(c.charAt(0)=='A')).collect(Collectors.toList());
+       for(String s: l2) System.out.print(s+" ");
 
+   }
 }
