4b6048f9c7f603f4eca2ff382da775aaa446cff7
Sirikonda Shiva <Sirikonda.Shiva@godigit.com>
2024-08-22 23:55:19 +0530

    used spring intializer created project with given dependencies

---
+++ spring-boot-22/src/main/java/com/sirikonda/spring_boot_22/SpringBoot22Application.java
@@ -0,0 +1,13 @@
+package com.sirikonda.spring_boot_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot22Application.class, args);
+	}
+
+}
